0aec3539e62a6660f818f90446ad3fe1d5efd4d5
pjwilson27 <pjwilson27@gmail.com>
2017-01-04 14:08:54 -0500

    Few adjustments to try and fix NG

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,9 @@
 (function(){
-    function HomeCtrl(){
-        var home = this;
+    function HomeCtrl(Room){
+        this.chatRooms = Room.getRooms().all;
     }
     
     angular
         .module('blocChat');
-        .controller('HomeCtrl',['$firebaseArray', HomeCtrl]);
+        .controller('HomeCtrl',['$firebaseArray', 'Room', HomeCtrl]);
 })
