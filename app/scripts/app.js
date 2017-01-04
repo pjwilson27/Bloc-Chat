5124a2b063c6dc04ed6b5c22d34d77f4890f3626
pjwilson27 <pjwilson27@gmail.com>
2017-01-04 09:39:01 -0500

    Implemented design..Need to fix injectors

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,23 @@
 (function(){
-    function config(){
+    function config($stateProvider, $locationProvider){
+        $locationProvider
+            .html5Mode({
+                enable: true,
+                requireBase: false
+            });
+            
+        $stateProvider
+            .state('home', {
+                   url: '/home',
+                   controller:'HomeCtrl as home',
+                   templateURL:'/templates/home.html'
+            });
+            
     
     
     }
     
     angular
-        .module('blocChat',['ui.router'],['firebase']);
+        .module('blocChat',['ui.router', 'firebase']);
         .config(config);
 })();
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,6 +1,6 @@
 (function(){
     function HomeCtrl(){
-        
+        this.mainContent = "Testing!"
     }
     
     angular
