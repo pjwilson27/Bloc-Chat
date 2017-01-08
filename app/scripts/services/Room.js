46f2dcc7c6dd47d67b28a02b0f755c1cdac4c90c
pjwilson27 <pjwilson27@gmail.com>
2017-01-08 09:43:17 -0500

    Setup files, DLd bootstrap

--- app/scripts/services/Room.js
+++ app/scripts/services/Room.js
@@ -3,10 +3,17 @@
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
         rooms = ["room1", "room2", "room3"];
+        rooms.$add({ rooms: "custom room" }).then(function(ref){
+            var addRooms = ref.Room;
+            console.log("Room has been successfully added");
+            list.$indexFor(ref);
+            
+        })
     
         return {
             all: rooms
         };
+        
     }
     
     angular
