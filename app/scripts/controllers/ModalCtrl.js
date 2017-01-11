26576efe2a306352d25bf16b1e76dcfc4e43138f
pjwilson27 <pjwilson27@gmail.com>
2017-01-11 16:17:51 -0500

    Asked for help..Need to connect modals

--- app/scripts/controllers/ModalCtrl.js
+++ app/scripts/controllers/ModalCtrl.js
@@ -1,9 +1,32 @@
-/*(function(){
-    function ModalCtrl(Modal){
+(function(){
+    function ModalCtrl($scope, $uibModal, Room){
+        this.room = Room;
         
+        this.open = function() {
+            $uibModal.open({
+                templateUrl: '/templates/Modal.html',
+                controller: 'ModalCtrl',
+                controllerAs: 'modal'
+            });
+        };
+        
+        this.close = function(){
+            console.log("Working");
+            if($scope.name === undefined){
+                $scope.$close();
+            }else{
+                this.room.create($scope, $scope.name);
+                $scope.$close();
+            }
+        };
+        
+        this.dismiss = function(){
+            console.log("Working");
+            $scope.$dismiss();
+        };
     }
     
     angular
         .module('blocChat')
-        .controller('ModalCtrl', ['Modal', ModalCtrl]);
+        .controller('ModalCtrl', ['$scope', '$uibModal', 'Room', ModalCtrl]);
 })();
--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,8 @@
 (function(){
-    function HomeCtrl(Room, $uibModal){
+    function HomeCtrl(Room/*, $uibModal*/){
         this.chatRooms = Room.all;
         
-        this.openModal = function() {
+ /*       this.openModal = function() {
             var instantModal = $uibModal.open({
                 templateUrl: '/templates/Modal.html',
                 controller: function($scope, $uibModalInstance) {
@@ -22,10 +22,10 @@
                 Room.addRoom(data);
             });
             console.log("weee");
-        };
+        };*/
     }
     
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$uibModal', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', /*'$uibModal',*/ HomeCtrl]);
 })();
