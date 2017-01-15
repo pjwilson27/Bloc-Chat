e7ab6f4b3f8218b4833575513af44b40cb913af0
pjwilson27 <pjwilson27@gmail.com>
2017-01-15 16:18:59 -0500

    Playing around with firebase/commands

--- dist/scripts/services/Room.js
+++ dist/scripts/services/Room.js
@@ -1,5 +1,6 @@
 (function(){
     function Room($firebaseArray){
+        var firebaseRef = new Firebase('https://bloc-chat-fab00.firebaseio.com');
         var ref = firebase.database().ref().child("rooms");
         var rooms = $firebaseArray(ref);
         var create = function($scope, name){
