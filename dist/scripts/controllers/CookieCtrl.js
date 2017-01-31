7b4916d7a48ff0fc4aef92d56ea289be906d0ec2
pjwilson27 <pjwilson27@gmail.com>
2017-01-31 15:37:50 -0500

    Fixed username -- fixing timestamp and styling

--- dist/scripts/controllers/CookieCtrl.js
+++ dist/scripts/controllers/CookieCtrl.js
@@ -1,19 +1,22 @@
 (function() {
-    function CookieCtrl($uibModalInstance, $cookies, Room) {
-        this.setUsername = function() {
+    function CookieCtrl($scope, $uibModalInstance, $cookies, Room) {
+        $scope.setUsername = function(username) {
             console.log(username);
-            if(this.username === undefined || null || ' ') {
-                alert("Please enter a username to continue!");
-                console.log("got to part 1");
-            } else {
-            $cookies.put('blocChatCurrentUser', this.username);
-            $uibModalInstance.close();
-            console.log("completed!")
+            if(username === null){
+                alert("You must enter something!");
+            } else if (username === undefined) {
+                alert("Seriously, just pick a damn username");
+            } else if (username === ''){
+                alert("Think you're clever leaving a blank? Cmon man.");
+            } else{
+                $cookies.put('blocChatCurrentUser', username);
+                $uibModalInstance.close();
+                console.log("completed!")
+            };
             }
-        }
     }
     
     angular
         .module('blocChat')
-        .controller('CookieCtrl', ['$uibModalInstance', '$cookies','Room', CookieCtrl]);
+        .controller('CookieCtrl', ['$scope','$uibModalInstance', '$cookies','Room', CookieCtrl]);
 })();
